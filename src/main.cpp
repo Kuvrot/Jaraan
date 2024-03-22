87be714017346f842ea5647fcc4c549d267e275f
Kuvrot <jamessanz@outlook.com>
2024-03-22 01:25:28 -0600

    formatting code

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,11 @@ void select(int n , Label lb) {
 
     switch (n) {
 
-    case 1: lb.setText("¡Hola amigo!"); break;
-    case 2: lb.setText("Hello friend!"); break;
-    case 3: lb.setText("Hallo freund!"); break;
-    case 4: lb.setText("Salve amici!"); break;
-
+        case 1: lb.setText("¡Hola amigo!"); break;
+        case 2: lb.setText("Hello friend!"); break;
+        case 3: lb.setText("Hallo freund!"); break;
+        case 4: lb.setText("Salve amici!"); break;
     }
-    
 }
 
 int main()
@@ -22,14 +20,9 @@ int main()
     Button btn3(1, 3, "Deutsch", 12, [label]() {select(3 , label); });
     Button btn4(1, 4, "Latin", 12, [label]() {select(4 , label); });
 
-    
-
     window.Update();
 
-
     return 0;
-
-    
 }
 
 
